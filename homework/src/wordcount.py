2e5658530c9faa54f152f72a489ae4619da2f2fc
yacardonama <yacardonama@unal.edu.co>
2025-05-03 10:44:43 -0500

    archivo por función y funciones a internals

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,33 +1,36 @@
 # obtain a list of files in the input directory
-import os
 
+from ._internals.count_words import count_words
+from ._internals.preprocess_lines import preprocess_lines
+from ._internals.read_all_lines import read_all_lines
+from ._internals.split_into_words import split_into_words
 from ._internals.write_count_words import write_count_words
-
-
-def read_all_lines():
-    all_lines = []
-    input_file_list = os.listdir("data/input/")
-    for file in input_file_list:
-        with open(file, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-            all_lines = lines.extend(lines)
-    return all_lines
 
 
 def main():
 
-    input_file_list = os.listdir("data/input/")
+    ## mover a la funcion "read_all_lines"
+    all_lines = read_all_lines()
+
+    ## mover a "preprocess_lines"
+    all_lines = preprocess_lines(all_lines)
+
+    ## mover "split_in_words"
+    words = split_into_words(all_lines)
+
+    ## mover a "count_words"
+    counter = count_words(words)
 
     # count the frequency of the words in the files in the input directory
-    counter = {}
-    for filename in input_file_list:
-        with open("data/input/" + filename) as f:
-            for l in f:
-                for w in l.split():
-                    w = w.lower().strip(",.!?")
-                    counter[w] = counter.get(w, 0) + 1
+    # counter = {}
+    # for filename in input_file_list:
+    #     with open("data/input/" + filename) as f:
+    #         for l in f:
+    #             for w in l.split():
+    #                 w = w.lower().strip(",.!?")
+    #                 counter[w] = counter.get(w, 0) + 1
 
-    # create the directory output/ if it doesn't exist
+    ##
     write_count_words(counter)
 
 
