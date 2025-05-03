1768997eb34e46b65b24cee75f3e981df397eaff
yacardonama <yacardonama@unal.edu.co>
2025-05-03 11:19:05 -0500

    cambio input y output folder

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -9,29 +9,14 @@ from ._internals.write_count_words import write_count_words
 
 def main():
 
-    ## mover a la funcion "read_all_lines"
-    all_lines = read_all_lines()
+    input_folder = "data/input/"
+    output_folder = "data/output"
 
-    ## mover a "preprocess_lines"
+    all_lines = read_all_lines(input_folder)
     all_lines = preprocess_lines(all_lines)
-
-    ## mover "split_in_words"
     words = split_into_words(all_lines)
-
-    ## mover a "count_words"
     counter = count_words(words)
-
-    # count the frequency of the words in the files in the input directory
-    # counter = {}
-    # for filename in input_file_list:
-    #     with open("data/input/" + filename) as f:
-    #         for l in f:
-    #             for w in l.split():
-    #                 w = w.lower().strip(",.!?")
-    #                 counter[w] = counter.get(w, 0) + 1
-
-    ##
-    write_count_words(counter)
+    write_count_words(counter, output_folder)
 
 
 if __name__ == "__main__":
